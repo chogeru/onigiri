0159e0521ce08152aa8d326e8e736ae835e73d88
kamano taiga <22051905@d.anabuki-college.net>
2022-10-05 14:43:23 +0900

    unnti

---
+++ ジョニー/Project/GameProject/Title.cpp
@@ -0,0 +1,39 @@
+
+#include "Title.h"
+#include "Game.h"
+Title::Title() :Base(eType_Scene),
+m_title_text("C:\\Windows\\Fonts\\msgothic.ttc", 64)
+{
+
+	m_img = COPY_RESOURCE("titlegamenn", CImage);
+	
+}
+
+Title::~Title()
+{
+	//タイトル破棄時にゲームシーンへ移行
+	Base::Add(new Game());
+}
+
+void Title::Update()
+{
+	//ボタン１でタイトル破棄
+	if (PUSH(CInput::eButton1)) {
+		m_kill = true;
+	}
+}
+
+void Title::Draw()
+{
+	m_img.Draw();
+
+	
+
+	//文字表示
+	//m_title_text.Draw(64, 256, 0, 0, 0, "title");
+	//m_title_text.Draw(64, 512, 0, 0, 0, "Push Z");
+}
+
+
+
+
---
+++ ジョニー/Project/GameProject/trapbullet.cpp
@@ -0,0 +1,28 @@
+#include "trapbullet.h"
+trapbullet::trapbullet(int type, const CVector2D& pos, float ang, float speed) : Base(type)
+{
+	m_img = COPY_RESOURCE("trapbullet", CImage);
+	
+
+	m_pos = pos;
+	m_img.SetCenter(16, 16);
+	m_ang = ang;
+	m_speed = speed;
+}
+
+void trapbullet::Update() {
+	m_vec = CVector2D(sin(m_ang), cos(m_ang)) * m_speed;
+	m_pos += m_vec;
+	if (PUSH(CInput::eMouseL)) {
+		Base::Add(new trapbullet(eType_trapbullet, m_pos, m_ang, 4));
+	}
+}
+void trapbullet::Draw() {
+	m_img.SetPos(m_pos);
+	m_img.SetAng(m_ang);
+	m_img.Draw();
+}
+
+void trapbullet::Collision(Base* b)
+{
+}
