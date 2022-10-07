ec42f1b606496e94b9e6c2b2b76e5ee097092abe
kitamura <xbeicun17@gmailcom>
2022-10-07 12:02:56 +0900

    a

--- ジョニー/Project/GameProject/Game.cpp
+++ ジョニー/Project/GameProject/Game.cpp
@@ -10,7 +10,7 @@ Game::Game() :Base(eType_Scene)
 	//Base::Add(new Player(CVector2D(0, 540), false));
 	//Base::Add(new Enemy(CVector2D(1280 + 256 * 1, 540), true));
 	//Base::Add(new Enemy(CVector2D(1280 + 256 * 2, 540), true));
-	Base::Add(new Enemy(CVector2D(1280 + 256 * 3, 540), true));
+	Base::Add(new Enemy(CVector2D(280 + 256 * 3, 440), true));
 
 
 }
