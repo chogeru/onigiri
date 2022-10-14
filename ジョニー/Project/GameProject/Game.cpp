d31154c4266b69d109db54715940c2036c82e057
takimotoayato <22051912@d.anabuki-college.net>
2022-10-14 14:31:26 +0900

    hennkou

--- ジョニー/Project/GameProject/Game.cpp
+++ ジョニー/Project/GameProject/Game.cpp
@@ -21,14 +21,20 @@ Game::Game() :Base(eType_Scene)
 }
 Game::~Game()
 {
-	/*//全てのオブジェクトを破棄
-	Base::KillAll();
+	//全てのオブジェクトを破棄
+	//Base::KillAll();
 	//タイトルシーンへ
-	Base::Add(new Title());*/
+	//Base::Add(new Title());
+
+	
+
+
 }
 
 void Game::Update()
 {
+
+	
 	//プレイヤー死亡　ボタン１でゲームシーン終了
 	if (!Base::FindObject(eType_Player))
 	{
