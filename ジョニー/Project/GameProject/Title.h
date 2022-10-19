a136061794b2ccc8b42aa2dc6caac66c8a11bfa5
takimotoayato <22051912@d.anabuki-college.net>
2022-10-19 11:50:58 +0900

    a

--- ジョニー/Project/GameProject/Title.h
+++ ジョニー/Project/GameProject/Title.h
@@ -9,6 +9,12 @@ class Title : public Base {
 	CImage m_text;
 	CImage m_text2;
 	CImage m_text3;
+	CImage m_text4;
+	CImage m_text5;
+	CImage m_text6;
+	CImage m_text7;
+	CImage m_text8;
+	CImage m_text9;
 
 	//文字表示オブジェクト
 	CFont m_title_text;
